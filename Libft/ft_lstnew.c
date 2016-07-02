8cfe073e17b38ac80dcfe6347e3def5340d40b52
David Dufour <David@pc5.home>
2016-07-02 18:29:45 +0200

    end

---
+++ Libft/ft_lstnew.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddufour <ddufour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/22 14:38:41 by ddufour           #+#    #+#             */
+/*   Updated: 2016/06/24 18:52:30 by ddufour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*list;
+
+	if ((list = (t_list*)malloc(sizeof(t_list))) == NULL)
+		return (NULL);
+	if (!content)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		list->content = ft_memalloc(content_size);
+		list->content = ft_memcpy(list->content, content, content_size);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
+}
